5055b8a6fe1ff4d1ac23ce24d6244a1a568a2d17
Peter Barker <53128103+Peter-Barker@users.noreply.github.com>
2019-07-21 19:19:40 +0100

    Update Password Safe.py

--- Password Safe.py
+++ Password Safe.py
@@ -1,5 +1,5 @@
 # This is a program which takes inputs consisting of a reference and password that the user wishes to record 
-# and writes them to an external folder
+# and writes them to an external txt file.
 
 # NOTE: ' os.system("cls") ' IS FOR WINDOWS ONLY
 # NOTE: ' os.system("clean") ' IS FOR UNBUNTU ONLY
@@ -14,29 +14,33 @@ password = input("Enter the password: ")
 if password == "peter":
   os.system("cls")
 else:
-    print("\n" "Incorrect password. Please try again.")
-    time.sleep(2)
+    print("\n" "Incorrect password. The program will automatically close now.")
+    time.sleep(3)
     quit()    
 
-# This section determines whether the user desires to reord a new REF/PASS or if they desire to view their records
-# If they want to view their stored passwords, it should display them, either in an external file or in the terminal itself
+# This section determines whether the user desires to insert a new record, if they desire to view their records or if they want to exit.
+# At the end of option 1 and 2 the program will return to the 'main menu' .
 
-print("Input a number to choose your action:" + "\n")
-input_1 = input("1 - view your records" + "\n" + "2 - add a new record" + "\n" + "3 - exit the program" + "\n" + ">")
-os.system("cls")
 
-if input_1 == "1": 
-  print("This is a list of your records:" + "\n")
-  f = open("Record.txt", "r")
-  print(f.read())
-elif input_1 == "2":
-  REFERENCE = input("Enter a reference that you wish to record: ")
-  PASSWORD = input("\n" "Enter the password you wish to record for this reference: ")
-  f = open("Record.txt", "a")
-  f.write("\n" + "Reference: " + REFERENCE + "\n" + "Password: " + PASSWORD + "\n" + "-" * 60)
-  f.close()
-else:
-  quit()
-
-print("")
-input("Press enter to close the program")
+while True:
+  print("Input a number to choose your action:" + "\n")
+  input_1 = input("1 - view your records" + "\n" + "2 - add a new record" + "\n" + "3 - exit the program" + "\n" + ">")
+  os.system("cls")
+  if input_1 == "1": 
+    print("Press enter to return to the menu.")
+    print("")
+    print("")
+    print("This is a list of your records:" + "\n")
+    f = open("Record.txt", "r")
+    print(f.read())
+    input("")
+    os.system("cls")
+  elif input_1 == "2":
+    REFERENCE = input("Enter a reference that you wish to record: ")
+    PASSWORD = input("\n" "Enter the password you wish to record for this reference: ")
+    f = open("Record.txt", "a")
+    f.write("-" * 60 + "\n" + "Reference: " + REFERENCE + "\n" + "Password: " + PASSWORD + "\n")
+    f.close()
+    os.system("cls")
+  else:
+    quit()
